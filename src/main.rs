e58d3cdba3c9c695920b0f444b8a5e14227a0722
Andrew Wason <rectalogic@rectalogic.com>
2025-10-06 17:07:55 -0400

    demo

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     app.run();
 }
 
-const BOX_DEPTH: f32 = 4.0;
+const WORLDBOX_DEPTH: f32 = 4.0;
 
 #[derive(Component)]
 struct WorldBox;
@@ -40,13 +40,63 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) -> Result<()> {
-    commands.spawn((Camera3d::default(), Transform::from_xyz(0., 0., BOX_DEPTH)));
-    let mut mesh = Mesh::from(Cuboid::new(0.5, 0.5, 0.5));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0., 0., WORLDBOX_DEPTH / 2.0),
+    ));
+    const WORLDBOX_SIZE: f32 = 1.0;
+    let mut mesh = Mesh::from(Cuboid::new(WORLDBOX_SIZE, WORLDBOX_SIZE, WORLDBOX_SIZE));
     invert_normals(&mut mesh)?;
     commands.spawn((
         WorldBox,
         Mesh3d(meshes.add(mesh)),
         MeshMaterial3d(materials.add(Color::from(basic::PURPLE))),
+        RigidBody::Static,
+        Collider::compound(vec![
+            // Ceiling
+            (
+                Position::from_xyz(0., WORLDBOX_SIZE / 2., 0.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::NEG_Y),
+            ),
+            // Floor
+            (
+                Position::from_xyz(0., -WORLDBOX_SIZE / 2., 0.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::Y),
+            ),
+            // Right wall
+            (
+                Position::from_xyz(WORLDBOX_SIZE / 2., 0., 0.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::NEG_X),
+            ),
+            // Left wall
+            (
+                Position::from_xyz(-WORLDBOX_SIZE / 2., 0., 0.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::X),
+            ),
+            // Back wall
+            (
+                Position::from_xyz(0., 0., WORLDBOX_SIZE / 2.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::NEG_Z),
+            ),
+            // Front wall
+            (
+                Position::from_xyz(0., 0., -WORLDBOX_SIZE / 2.),
+                Quat::IDENTITY,
+                Collider::half_space(Vec3::Z),
+            ),
+        ]),
+    ));
+
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::cuboid(0.25, 0.25, 0.25),
+        Mesh3d(meshes.add(Cuboid::new(0.25, 0.25, 0.25))),
+        MeshMaterial3d(materials.add(Color::from(basic::YELLOW))),
     ));
     Ok(())
 }
@@ -57,10 +107,11 @@ fn on_resize(
     worldbox: Single<&mut Transform, With<WorldBox>>,
 ) {
     if resize_reader.read().last().is_some() {
-        let [_, topright, ..] = projection.get_frustum_corners(BOX_DEPTH, BOX_DEPTH * 2.);
+        let [_, topright, ..] = projection.get_frustum_corners(WORLDBOX_DEPTH, WORLDBOX_DEPTH + 1.);
         let mut transform = worldbox.into_inner();
-        transform.translation = Vec3::new(0., 0., topright.z - BOX_DEPTH / 2.0);
-        transform.scale = Vec3::new(topright.x, topright.y, BOX_DEPTH);
+        // transform.translation = Vec3::new(0., 0., -(topright.z - WORLDBOX_DEPTH / 2.0));
+        transform.translation = Vec3::new(0., 0., -topright.z / 2.0);
+        transform.scale = Vec3::new(topright.x, topright.y, WORLDBOX_DEPTH);
     }
 }
 
