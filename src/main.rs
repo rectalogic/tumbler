e255e07dcef3e65a9d49147fb244844e9d3f2a45
Andrew Wason <rectalogic@rectalogic.com>
2025-10-07 11:46:13 -0400

    parent WorldBox under Camera

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use std::f32::consts::PI;
+
 use avian3d::prelude::*;
 use bevy::{
-    camera::CameraProjection,
     color::palettes::basic,
     mesh::VertexAttributeValues,
     prelude::*,
-    window::{WindowMode, WindowResized, WindowResolution},
+    window::{WindowResized, WindowResolution},
 };
 
 fn main() {
@@ -25,7 +26,7 @@ fn main() {
         PhysicsPlugins::default(),
     ))
     .add_systems(Startup, setup)
-    .add_systems(Update, on_resize);
+    .add_systems(Last, on_resize);
 
     app.run();
 }
@@ -40,78 +41,87 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) -> Result<()> {
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0., 0., WORLDBOX_DEPTH / 2.0),
-    ));
     const WORLDBOX_SIZE: f32 = 1.0;
     let mut mesh = Mesh::from(Cuboid::new(WORLDBOX_SIZE, WORLDBOX_SIZE, WORLDBOX_SIZE));
     invert_normals(&mut mesh)?;
     commands.spawn((
-        WorldBox,
-        Mesh3d(meshes.add(mesh)),
-        MeshMaterial3d(materials.add(Color::from(basic::PURPLE))),
-        RigidBody::Static,
-        Collider::compound(vec![
-            // Ceiling
-            (
-                Position::from_xyz(0., WORLDBOX_SIZE / 2., 0.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::NEG_Y),
-            ),
-            // Floor
-            (
-                Position::from_xyz(0., -WORLDBOX_SIZE / 2., 0.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::Y),
-            ),
-            // Right wall
-            (
-                Position::from_xyz(WORLDBOX_SIZE / 2., 0., 0.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::NEG_X),
-            ),
-            // Left wall
-            (
-                Position::from_xyz(-WORLDBOX_SIZE / 2., 0., 0.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::X),
-            ),
-            // Back wall
-            (
-                Position::from_xyz(0., 0., WORLDBOX_SIZE / 2.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::NEG_Z),
-            ),
-            // Front wall
-            (
-                Position::from_xyz(0., 0., -WORLDBOX_SIZE / 2.),
-                Quat::IDENTITY,
-                Collider::half_space(Vec3::Z),
-            ),
-        ]),
+        Camera3d::default(),
+        Transform::from_xyz(-1., 2., 3.).with_rotation(Quat::from_axis_angle(Vec3::Y, PI)), //XXX random
+        RigidBody::Kinematic,
+        children![(
+            WorldBox,
+            Mesh3d(meshes.add(mesh)),
+            MeshMaterial3d(materials.add(Color::from(basic::PURPLE))),
+            Collider::compound(vec![
+                // Ceiling
+                (
+                    Position::from_xyz(0., WORLDBOX_SIZE / 2., 0.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::NEG_Y),
+                ),
+                // Floor
+                (
+                    Position::from_xyz(0., -WORLDBOX_SIZE / 2., 0.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::Y),
+                ),
+                // Right wall
+                (
+                    Position::from_xyz(WORLDBOX_SIZE / 2., 0., 0.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::NEG_X),
+                ),
+                // Left wall
+                (
+                    Position::from_xyz(-WORLDBOX_SIZE / 2., 0., 0.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::X),
+                ),
+                // Back wall
+                (
+                    Position::from_xyz(0., 0., WORLDBOX_SIZE / 2.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::NEG_Z),
+                ),
+                // Front wall
+                (
+                    Position::from_xyz(0., 0., -WORLDBOX_SIZE / 2.),
+                    Quat::IDENTITY,
+                    Collider::half_space(Vec3::Z),
+                ),
+            ])
+        )],
     ));
 
-    commands.spawn((
-        RigidBody::Dynamic,
-        Collider::cuboid(0.1, 0.1, 0.1),
-        Mesh3d(meshes.add(Cuboid::new(0.1, 0.1, 0.1))),
-        MeshMaterial3d(materials.add(Color::from(basic::YELLOW))),
-    ));
     Ok(())
 }
 
 fn on_resize(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     mut resize_reader: MessageReader<WindowResized>,
-    projection: Single<&Projection, With<Camera>>,
+    camera: Single<(&Projection, &GlobalTransform), With<Camera>>,
     worldbox: Single<&mut Transform, With<WorldBox>>,
 ) {
     if resize_reader.read().last().is_some() {
+        let (projection, camera_global_transform) = camera.into_inner();
         let [_, topright, ..] = projection.get_frustum_corners(WORLDBOX_DEPTH, WORLDBOX_DEPTH + 1.);
-        let mut transform = worldbox.into_inner();
-        // transform.translation = Vec3::new(0., 0., -(topright.z - WORLDBOX_DEPTH / 2.0));
-        transform.translation = Vec3::new(0., 0., -topright.z / 2.0);
-        transform.scale = Vec3::new(topright.x, topright.y, WORLDBOX_DEPTH);
+        let mut worldbox_transform = worldbox.into_inner();
+        worldbox_transform.translation = Vec3::new(0., 0., -topright.z / 2.0);
+        worldbox_transform.scale = Vec3::new(topright.x, topright.y, WORLDBOX_DEPTH);
+
+        // XXX spawn a test cube inside the transformed worldbox
+        commands.spawn((
+            RigidBody::Dynamic,
+            Collider::cuboid(0.1, 0.1, 0.1),
+            Mesh3d(meshes.add(Cuboid::new(0.1, 0.1, 0.1))),
+            MeshMaterial3d(materials.add(Color::from(basic::YELLOW))),
+            Transform::from_translation(
+                camera_global_transform
+                    .transform_point(worldbox_transform.transform_point(Vec3::ZERO)),
+            ),
+        ));
     }
 }
 
